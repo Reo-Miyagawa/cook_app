90eb2961b7409e155147fd6321920a19744434b8
Reo-Miyagawa <atethxhrimatggg@gmail.com>
2020-11-26 16:56:15 +0900

    userモデルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :name
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'Include both letters and numbers' }
+    #半角英数字混合
+  end
+
+  has_many :cooks
+  has_many :comments
+  
 end
